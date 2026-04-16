e101b666276a1e03cd20f1715c222d53ec5e6e0f
it <ittphon1995@gmail.com>
2026-04-16 13:06:55 +0700

    feat(crop): pre-cache /crop route in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'plang-roop-v3';
+const CACHE_NAME = 'plang-roop-v4';
 const STATIC_ASSETS = [
   '/',
   '/editor',
   '/batch',
   '/favicon-generator',
+  '/crop',
 ];
 
 // Install event - cache static assets
